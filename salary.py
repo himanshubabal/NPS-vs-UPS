2fdaaa5dc449c6c67667b7786efd648840810790
Himanshu Babal <himanshubabal@users.noreply.github.com>
2025-05-24 02:44:41 +0530

    salary done

--- salary.py
+++ salary.py
@@ -5,6 +5,8 @@ from helper_functions import *
 import pprint
 
 from typing import Union
+from calendar import monthrange
+
 
 def get_salary_matrix(starting_level:Union[int,str] = 10, starting_year_in_level:int = 1, promotion_duration_array:list[int] = [4, 5, 4, 1, 4, 7, 5, 3], 
                        present_pay_matrix_csv:str = '7th_CPC.csv', dob:str = '20/5/1996', doj:str = '9/12/24', is_ias:bool = False,
@@ -56,25 +58,56 @@ def get_salary_matrix(starting_level:Union[int,str] = 10, starting_year_in_level
 
     return salary_matrix
 
-def get_monthly_salary(salary_matrix, joining_year, retirement_year):
+def get_monthly_salary(salary_matrix, dob:str = '20/5/1996', doj:str = '9/12/24'):
+    # Imp dates
+    joining = parse_date(doj)
+    retirement = get_retirement_date(dob)
+
     # Init empty dictionary for years in range joining year to retire year
-    salary_monthly_detailed_matrix = {year: {} for year in range(int(joining_year), int(retirement_year)+1)}
+    salary_monthly_detailed_matrix = {year: {} for year in range(int(joining.year), int(retirement.year)+1)}
     for year, six_monthly_salary in salary_matrix.items():
         month_dict = {}
-        if year % 1 == 0.5:     # year ending with 0.5 -> means second half of year -> month 7 to 12
-            month_dict = {key: six_monthly_salary for key in range(7,13)}
-        if year % 1 != 0.5:     # year ending with 0.0 -> means first half of year -> month 1 to 6
-            month_dict = {key: six_monthly_salary for key in range(1,7)}
+        
+        # Getting month list for given half-year
+        if year % 1 == 0.5:
+            months_range = range(7, 13)  # Second half -- year ending with 0.5 -> means second half of year -> month 7 to 12
+        else:
+            months_range = range(1, 7)   # First half -- year ending with 0.0 -> means first half of year -> month 1 to 6
+
+        # Since only integer part (0.5 removed) is required from hereon
+        year = int(year)
+        for month in months_range:
+            # Check if month is before joining month in joining year or after retirement month in retirement year, make their salary zero
+            if (year == joining.year and month < joining.month) or (year == retirement.year and month > retirement.month):
+                adjusted_salary = 0
+            else:
+                adjusted_salary = six_monthly_salary
+
+                # Adjust first month salary proportionally to days served
+                if year == joining.year and month == joining.month:
+                    days_in_month = monthrange(year, month)[1]
+                    served_days = days_in_month - joining.day + 1
+                    adjusted_salary = int(six_monthly_salary * (served_days / days_in_month))
+                    print(f'Year: {year}, Month: {month}, Days Served: {served_days}/{days_in_month}, Salary: {adjusted_salary}')
+
+                # Adjust last month salary proportionally to days served
+                if year == retirement.year and month == retirement.month:
+                    days_in_month = monthrange(year, month)[1]
+                    served_days = retirement.day
+                    adjusted_salary = int(six_monthly_salary * (served_days / days_in_month))
+                    print(f'Year: {year}, Month: {month}, Days Served: {served_days}/{days_in_month}, Salary: {adjusted_salary}')
+
+            month_dict[month] = adjusted_salary
         
         # Add months dict as value for year key (int makes year round off)
-        salary_monthly_detailed_matrix[int(year)].update(month_dict)
+        salary_monthly_detailed_matrix[year].update(month_dict)
     
     return salary_monthly_detailed_matrix
 
 if __name__ == "__main__":
     salary_matrix = get_salary_matrix()
-    salary_monthly_detailed_matrix = get_monthly_salary(salary_matrix, 2024, 2056)
+    salary_monthly_detailed_matrix = get_monthly_salary(salary_matrix)
 
-    pprint.pprint(salary_matrix)
+    # pprint.pprint(salary_matrix)
     pprint.pprint(salary_monthly_detailed_matrix)
     
